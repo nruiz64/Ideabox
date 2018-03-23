b62a366484ac4809b1c6b0cfec09107a06c68aeb
nruiz64 <ruiz.nicolas64@gmail.com>
2018-03-23 23:07:33 +0100

    add upvote/dowvote

--- src/components/App.js
+++ src/components/App.js
@@ -1,52 +1,53 @@
-import React, { PureComponent } from "react";
-import Card from "./Card";
-import CardForm from "./CardForm";
-import axios from "axios";
-import { api } from "../constant.json";
+import React, { PureComponent } from 'react';
+import Card from './Card';
+import CardForm from './CardForm';
+import axios from 'axios';
+import { api } from '../constant.json';
 
 class App extends PureComponent {
-  constructor(props) {
-    super(props);
-    this.state = {
-      ideas: []
-    };
-  }
+	constructor(props) {
+		super(props);
+		this.state = {
+			ideas: []
+		};
+	}
 
-  componentDidMount() {
-    axios
-      .get(`${api}/idea/all`)
-      .then(({ data }) => {
-        this.setState({ ideas: data });
-      })
-      .catch(error => {
-        console.log(error);
-      });
-  }
+	componentDidMount() {
+		axios
+			.get(`${api}/idea/all`)
+			.then(({ data }) => {
+				this.setState({ ideas: data });
+			})
+			.catch((error) => {
+				console.log(error);
+			});
+	}
 
-  addNewIdea = newIdea => {
-    axios
-      .post(`${api}/idea/`, newIdea)
-      .then(({ data }) => {
-        const newIdeas = [...this.state.ideas, data];
-        this.setState({ ideas: newIdeas });
-      })
-      .catch(error => {
-        console.log(error);
-      });
-  };
+	addNewIdea = (newIdea) => {
+		axios
+			.post(`${api}/idea/`, newIdea)
+			.then(({ data }) => {
+				console.log(data);
+				const newIdeas = [ ...this.state.ideas, data ];
+				this.setState({ ideas: newIdeas });
+			})
+			.catch((error) => {
+				console.log(error);
+			});
+	};
 
-  render() {
-    const { ideas } = this.state;
-    return (
-      <div className="uk-container uk-margin-large-top">
-        <CardForm addNewIdea={this.addNewIdea} />
+	render() {
+		const { ideas } = this.state;
+		return (
+			<div className="uk-container uk-margin-small-top">
+				<CardForm addNewIdea={this.addNewIdea} />
 
-        {Object.keys(ideas).map(key => {
-          return <Card idea={ideas[key]} key={key} />;
-        })}
-      </div>
-    );
-  }
+				{Object.keys(ideas).map((key) => {
+					return <Card idea={ideas[key]} key={key} upvote={this.upvote} />;
+				})}
+			</div>
+		);
+	}
 }
 
 export default App;
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,20 +1,46 @@
-import React, { PureComponent } from "react";
+import React, { PureComponent } from 'react';
+import axios from 'axios';
+import { api } from '../constant.json';
 
 export default class Card extends PureComponent {
-  render() {
-    const { idea } = this.props;
-    const { title, description, asker, accepted } = idea;
-    return (
-      <div className="uk-card uk-card-default uk-card-body uk-width-1-2@m margin-top-5 ">
-        <h3 className="uk-card-title">
-          {asker} :
-          <em>
-            {title} <span uk-icon="plus" />
-            <span uk-icon="minus" />
-          </em>
-        </h3>
-        <p>{description}</p>
-      </div>
-    );
-  }
+	state = {
+		data: {}
+	};
+	upvote = (e) => {
+		axios
+			.get(`${api}/idea/upvote/${this.props.idea.id}`)
+			.then(({ data }) => {
+				this.setState({ data });
+			})
+			.catch((error) => {
+				console.log(error);
+			});
+	};
+
+	downvote = (e) => {
+		axios
+			.get(`${api}/idea/downvote/${this.props.idea.id}`)
+			.then(({ data }) => {
+				this.setState({ data });
+			})
+			.catch((error) => {
+				console.log(error);
+			});
+	};
+	render() {
+		const { idea } = this.props;
+		const { title, description, asker, id, vote } = idea;
+		return (
+			<div className="uk-card uk-card-default uk-card-body uk-width-1-1@m margin-top-5 custom-card">
+				<h3 className="uk-card-title">
+					{asker} :&nbsp;
+					<em>{title}</em>&nbsp;
+					<button uk-icon="arrow-up" onClick={this.upvote} />&nbsp;
+					<button uk-icon="arrow-down" onClick={this.downvote} />&nbsp; <span uk-icon="arrow-right" />
+					&nbsp;{this.state.data.vote != null ? this.state.data.vote : vote}
+				</h3>
+				<p>{description}</p>
+			</div>
+		);
+	}
 }
